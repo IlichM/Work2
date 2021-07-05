cc7cdc55982cf7b3637fdb628c5c06c4b784baa9
viruslm <viruslm@mail.ru>
2021-07-05 23:13:25 +0300

    1 commit

---
+++ src/ListStringsConvertToListNumbers/StandartMethods.java
@@ -0,0 +1,11 @@
+package ListStringsConvertToListNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StandartMethods {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("1","2","3","4","5");
+        List<>
+    }
+}
